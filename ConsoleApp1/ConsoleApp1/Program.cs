69a43c2059affffc3ef86137deae613b9cadf571
Laim0N <Laim0n1995@gmail.com>
2024-10-25 14:30:42 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Добро пожаловать в генератор персонажа!");
+
+        Console.Write("Введите имя персонажа: ");
+        string? name = Console.ReadLine();
+
+        Console.Write("Введите возраст: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите класс персонажа (Воин/Маг/Лучник): ");
+        string? classChar = Console.ReadLine();
+
+        Random rnd = new Random();
+        int isHealthChar = rnd.Next(80, 100);
+        int isStrChar = rnd.Next(10, 20);
+
+        Console.WriteLine("Создан новый персонаж:/n");
+        Console.WriteLine($"Имя: {name}");
+        Console.WriteLine($"Возраст: {age}");
+        Console.WriteLine($"Класс: {classChar}");
+        Console.WriteLine($"Ваш персонаж имеет {isHealthChar} здоровья и {isStrChar} силы.");
+
+    }
+}
