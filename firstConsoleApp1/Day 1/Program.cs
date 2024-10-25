57d3ad36ad9998febed9fa972865cfd094719a07
Laim0N <Laim0n1995@gmail.com>
2024-10-25 16:59:21 +1000

    После выбора класса персонажу присваеватся оружие.

--- firstConsoleApp1/Day 1/Program.cs
+++ firstConsoleApp1/Day 1/Program.cs
@@ -14,25 +14,43 @@ class Program
 
         Console.Write("Введите класс персонажа (Воин/Маг/Лучник): ");
         string? classChar = Console.ReadLine();
+        string weapon;
         if (classChar.ToLower() == "воин")
         {
-            Console()
+            weapon = "меч и щит";
+            Console.Write($"Твоим оружием будет {weapon}.");
+
+        } else if (classChar.ToLower() == "маг")
+        {
+            weapon = "посох";
+            Console.Write($"Твоим оружием будет {weapon}.");
+
+        } else if (classChar.ToLower() == "лучник")
+        {
+            weapon = "лук";
+            Console.Write($"Твоим оружием будет {weapon}.");
+        }
+        else
+        {
+            weapon = "палка";
+            Console.Write($"Ты не выбрал класс, теперь у тебя есть {weapon}. Хех, удачи!");
+
         }
 
         Random rnd = new Random();
         int isHealthChar = rnd.Next(80, 100);
         int isStrChar = rnd.Next(10, 20);
 
-        Console.WriteLine("Создан новый персонаж:/n");
+        Console.WriteLine("Создан новый персонаж:\n");
         Console.WriteLine($"Имя: {name}");
         Console.WriteLine($"Возраст: {age}");
         Console.WriteLine($"Класс: {classChar}");
         Console.WriteLine($"Ваш персонаж имеет {isHealthChar} здоровья и {isStrChar} силы.");
 
-        if (isStrChar > 90)
-            Console.WriteLine("ВАУ! Какой вы сильный, вы будете сметать врагов пачками!!!");
-        else 
-            Console.WriteLine("Ой... А вы точно сможете "
+        if (isStrChar >= 90)
+            Console.WriteLine("ВАУ! Какой ты сильный, ты будешь сметать врагов пачками!!!");
+        else
+            Console.WriteLine($"Ой... А ты точно сможешь поднять {weapon}");
 
     }
 }
