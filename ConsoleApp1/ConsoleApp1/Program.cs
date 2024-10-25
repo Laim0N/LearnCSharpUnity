b4212ad3c794ab9087d7151b16c893d498b76233
Laim0N <Laim0n1995@gmail.com>
2024-10-25 16:20:22 +1000

    Добавил характеристики персонажа, их генерацию.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,6 +14,10 @@ class Program
 
         Console.Write("Введите класс персонажа (Воин/Маг/Лучник): ");
         string? classChar = Console.ReadLine();
+        if (classChar.ToLower() == "воин")
+        {
+            Console()
+        }
 
         Random rnd = new Random();
         int isHealthChar = rnd.Next(80, 100);
@@ -24,6 +28,11 @@ class Program
         Console.WriteLine($"Возраст: {age}");
         Console.WriteLine($"Класс: {classChar}");
         Console.WriteLine($"Ваш персонаж имеет {isHealthChar} здоровья и {isStrChar} силы.");
+
+        if (isStrChar > 90)
+            Console.WriteLine("ВАУ! Какой вы сильный, вы будете сметать врагов пачками!!!");
+        else 
+            Console.WriteLine("Ой... А вы точно сможете "
 
     }
 }
